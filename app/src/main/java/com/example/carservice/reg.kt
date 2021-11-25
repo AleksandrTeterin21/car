f6493aa19d62d9ed84535e0e536bd62187dabbcf
КУПК ИТ <kupc_visual@outlook.com>
2021-11-26 01:11:58 +0500

    ghjk

--- app/src/main/java/com/example/carservice/reg.kt
+++ app/src/main/java/com/example/carservice/reg.kt
@@ -3,16 +3,62 @@ package com.example.carservice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Patterns
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import java.util.regex.Pattern.compile
 
 class reg : AppCompatActivity() {
+    lateinit var email: EditText
+    lateinit var password:EditText
+    lateinit var pattern:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg)
+        email = findViewById(R.id.email_login)
+        password = findViewById(R.id.password_login)
+        pattern =("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,50}" +
+                "\\@" +
+                "[a-z][a-z\\-]{0,8}" +
+                "(" +
+                "\\." +
+                "[a-z][a-z\\-]{0,5}" +
+                ")+")
+
+    }
+    fun EmailValid(email1:String): Boolean {return compile(pattern).matcher(email1).matches()}
+    fun login(view: View) {
+        if (email.text.isNotEmpty() && password.text.isNotEmpty()) {
+            if (EmailValid(email.text.toString())) {
+Toast.makeText(this,"dfffff",Toast.LENGTH_SHORT).show()
+                val inten = Intent(this,Menu1::class.java)
+                startActivity(intent)
+            }
+            else{
+                val alert2 = AlertDialog.Builder(this)
+                    .setTitle("Ошибка в поле email")
+                    .setMessage("Проверьте поле email на правилность ввода")
+                    .setNegativeButton("ok",null)
+                    .create()
+                    .show()
+            }
+        }
+        else {
+            val alert = AlertDialog.Builder(this)
+                .setTitle("Ошибка входа")
+                .setMessage("У вас есть пустые поля")
+                .setPositiveButton("ok",null)
+                .create()
+                .show()
+
+        }
     }
 
-    fun login(view: View) {
-        val intent = Intent(this@reg, Menu1::class.java)
+    fun registr(view: View) {
+        val intent = Intent(this,SignUpActivity::class.java)
         startActivity(intent)
     }
 
